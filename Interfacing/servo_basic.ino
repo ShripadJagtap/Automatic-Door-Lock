ba9e460dc23d80df0eff6d5a1a0ce520ce1f0e69
Shripad Jagtap <70714209+ShripadJagtap@users.noreply.github.com>
2021-10-24 13:29:06 +0530

    Update servo_basic.ino

--- Interfacing/servo_basic.ino
+++ Interfacing/servo_basic.ino
@@ -1,1 +1,26 @@
 //Contributed by ShripadJagtap & Phantonym21
+#include<Servo.h>
+Servo Myservo;
+int pos;
+void setup()
+{
+Myservo.attach(3);
+}
+
+void loop()
+{
+  
+  
+for(pos=0;pos<=180;pos++){
+Myservo.write(pos);
+delay(15);
+}
+  delay(1000);
+  
+  for(pos=180;pos>=0;pos--){
+Myservo.write(pos);
+delay(15);
+}
+  delay(1000);
+  
+}
